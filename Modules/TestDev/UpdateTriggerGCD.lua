6b06dac4feb57089ee789af6fc6a87df68096120
MisterCrab <51509374+MisterCrab@users.noreply.github.com>
2019-09-21 02:24:58 +0300

    Update TriggerGCD

--- Modules/TestDev/UpdateTriggerGCD.lua
+++ Modules/TestDev/UpdateTriggerGCD.lua
@@ -4,27 +4,33 @@ local GetSpellTexture						= TMW.GetSpellTexture
 local A 									= Action
 local GetSpellInfo							= A.GetSpellInfo
 local print 								= A.Print
+local wipe									= wipe
+local tostringall							= tostringall
 
-local GetSpellSubtext, GetSpellBaseCooldown	= 
-	  GetSpellSubtext. GetSpellBaseCooldown
+local GetSpellBaseCooldown				 	= GetSpellBaseCooldown
 
 -- Classic version
 function ClassicTriggerGCD()
-	TMW.db.global.TriggerGCD = nil
+	if TMW.db.profiles.TriggerGCD then 
+		wipe(TMW.db.profiles.TriggerGCD)
+	else 
+		TMW.db.profiles.TriggerGCD = {}
+	end 
+	
 	local temp = {}
 	for i = 1, 900000 do 
 		local spellName, _, spellTexture, _, _, _, spellID = GetSpellInfo(i)
-		if spellName and spellID == i and GetSpellTexture(spellID) then 
-			local isPlayerSpell = GetSpellSubtext(spellID)
-			if isPlayerSpell and isPlayerSpell ~= "" then 
+		if spellName and spellID == i and spellName:find("[\128-\255]") and GetSpellTexture(spellID) then 
+			local isPlayerSpell = GetSpellDescription(spellID)
+			if isPlayerSpell then 
 				local base, baseGCD = GetSpellBaseCooldown(spellID)
 				if base and baseGCD then 
-					temp[spellID] = baseGCD
+					TMW.db.profiles.TriggerGCD[tostringall(i .. "	")] = baseGCD
+					-- RegEx (\d)\s("]) => $1]
 				end 
 			end 
 		end 
 	end 
 	
-	TMW.db.global.TriggerGCD = temp
 	print("TriggerGCD updated!")
 end 
